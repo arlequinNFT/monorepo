53868ed3256ac83a5fc09f437a23424cc7de9820
Kevin Taleb Dehkordi <kevin.taleb.dehkordi@gmail.com>
2022-01-24 15:32:20 +0100

    fix responsive

--- apps/web/components/what-is-arlequin-mobile/what-is-arlequin-mobile.component.tsx
+++ apps/web/components/what-is-arlequin-mobile/what-is-arlequin-mobile.component.tsx
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer';
 const WhatIsArlequinMobile = () => {
   return (
     <section className="grid gap-y-6 p-6">
-      <div className="flex flex-col justify-center p-6 text-center shadow-lg rounded-3xl bg-white">
+      <div className="flex flex-col justify-center py-6 text-center shadow-lg rounded-3xl bg-white">
         <img
           width="100%"
           height="100%"
@@ -17,12 +17,12 @@ const WhatIsArlequinMobile = () => {
         <p className="font-bold text-3xl my-4 text-red uppercase">
           Cute 3D animals living in Arlequin Metaverse
         </p>
-        <p className="text-2xl">
+        <p className="px-4 text-2xl">
           Dozens species exist, and we are introducing one each week!
         </p>
       </div>
 
-      <div className="flex flex-col justify-center p-6 text-center shadow-lg rounded-3xl bg-white">
+      <div className="flex flex-col justify-center py-6 text-center shadow-lg rounded-3xl bg-white">
         <img
           width="100%"
           height="100%"
@@ -35,13 +35,13 @@ const WhatIsArlequinMobile = () => {
         <p className="font-bold text-3xl my-4 text-red uppercase">
           Arlequin is the Artists Metaverse
         </p>
-        <p className="text-2xl">
+        <p className="px-4 text-2xl">
           Add your personnal artistic touch by painting on the Arlees using
           Arlequin's Painter.
         </p>
       </div>
 
-      <div className="flex flex-col justify-center p-6 text-center shadow-lg rounded-3xl bg-white">
+      <div className="flex flex-col justify-center py-6 text-center shadow-lg rounded-3xl bg-white">
         <img
           width="100%"
           height="100%"
@@ -54,13 +54,13 @@ const WhatIsArlequinMobile = () => {
         <p className="font-bold text-3xl my-4 text-red uppercase">
           A unique way to earn with your art
         </p>
-        <p className="text-2xl">
+        <p className="px-4 text-2xl">
           Participate to our painting contests, climb the leaderboard, win NFTs
           and FUSD! <br />
         </p>
       </div>
 
-      <div className="flex flex-col justify-center p-6 text-center shadow-lg rounded-3xl bg-white">
+      <div className="flex flex-col justify-center py-6 text-center shadow-lg rounded-3xl bg-white">
         <img
           width="100%"
           height="100%"
@@ -74,27 +74,27 @@ const WhatIsArlequinMobile = () => {
         <p className="font-bold text-3xl my-4 text-red uppercase">
           Arlequin is fully community driven
         </p>
-        <p className="text-2xl">
+        <p className="px-4 text-2xl">
           If you are an Arlee adopter, you can vote for your preferred Arlees
           entries! <br />
           Each vote makes you earn Nimo!
         </p>
       </div>
 
-      <div className="flex flex-col justify-center p-6 text-center shadow-lg rounded-3xl bg-white">
+      <div className="flex flex-col justify-center py-6 text-center shadow-lg rounded-3xl bg-white">
         <img width="100%" height="100%" src="/images/nimo.webp" alt="Nimo" />
         <p className="font-extrabold text-6xl text-primary uppercase">Nimo</p>
 
         <p className="font-bold text-3xl my-4 text-red uppercase">
           the official Arlequin currency
         </p>
-        <p className="text-2xl">
+        <p className="px-4 text-2xl">
           Buy cosmetics items, poses for your Arlees, backgrounds, level up your
           Arlees and more with Nimo!
         </p>
       </div>
 
-      <div className="flex flex-col justify-center p-6 text-center shadow-lg rounded-3xl bg-white">
+      <div className="flex flex-col justify-center py-6 text-center shadow-lg rounded-3xl bg-white">
         <img
           width="100%"
           height="100%"
@@ -108,7 +108,7 @@ const WhatIsArlequinMobile = () => {
         <p className="font-bold text-3xl my-4 text-red uppercase">
           Arlequin is engaged in Animal care
         </p>
-        <p className="text-2xl">
+        <p className="px-4 text-2xl">
           Each time an Arlee is bought from the Pool, 5% of the revenue goes to
           an animal charity according to the Arlee species
         </p>
