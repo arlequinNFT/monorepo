4d50b998b4f96f62bb12c101f8b9fcca75f157c1
KevTale <kevin.taleb.dehkordi@gmail.com>
2022-04-06 15:12:37 +0200

    wip

--- apps/painter/configs/web3.ts
+++ apps/painter/configs/web3.ts
@@ -1,5 +1,6 @@
 import { Web3Storage } from 'web3.storage';
 
 export const web3Client = new Web3Storage({
-  token: process.env.NEXT_PUBLIC_WEB3_TOKEN,
+  token:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDdkRTY1OTMxNzc5OTMyZDFFM0FhQ2VGMkU4Q2IyQUQ1MUQ1M2NDQjQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDkwOTg4MDkxOTMsIm5hbWUiOiJBcmxlcXVpbiJ9.91TbSNuEdmMNKDObvQsm7OwAw5qp_IrvkUVUIqJXYG4',
 });
--- apps/painter/pages/index.tsx
+++ apps/painter/pages/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import ReactTooltip from 'react-tooltip';
 import Unity, { UnityContext } from 'react-unity-webgl';
+import { NFTStorage } from 'nft.storage';
+
 import * as fcl from '@onflow/fcl';
 import { ComponentsButton } from '@arlequin/components/button';
 import ArleesMode from '../components/arlees-mode/arlees-mode.component';
@@ -44,6 +46,7 @@ import Partners from '../components/partners/partners.component';
 import { MINT_ARLEE_SCENE_NFT } from '../cadence/transactions/ArleeScene/mintArleeSceneNFT';
 import { web3Client } from '../configs/web3';
 import { setCurrentUser } from '../store/reducers/auth.reducer';
+import { Web3Storage } from 'web3.storage';
 const Index: NextPage = () => {
   const { keyPress } = useScrollDirection();
 
@@ -86,6 +89,11 @@ const Index: NextPage = () => {
     } else {
       fcl.logIn();
     }
+
+    const files = [
+      // new File([thumbnail], 'thumbnail.png'),
+      new File(['contents-of-file-1'], 'plain-utf8.txt'),
+    ];
   };
 
   //#region Use Effects
@@ -119,34 +127,34 @@ const Index: NextPage = () => {
 
   useEffect(() => {
     if (unityContext) {
-      unityContext?.on('SendArtwork', async (json, thumbnail: string) => {
-        console.log({ json });
-        console.log({ thumbnail });
+      unityContext?.on(
+        'SendArtwork',
+        async (metadata: string, thumbnail: string) => {
+          console.log({ metadata });
+          console.log({ thumbnail });
 
-        // const metadata = new Blob([JSON.stringify(json)], {
-        //   type: 'application/json',
-        // });
+          const nftstorage = new NFTStorage({
+            token:
+              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDU4RTUwRWRCN0U2YWFlOGJiNDVDQjU3ZEVmMmRiYzQ3ODhDNGNjRjYiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0OTI0OTQzMzQyOSwibmFtZSI6ImFybGVxdWluLWRldiJ9.90n0AFzQxuZIKTNXras_K9arSf8CdGsQNySf6vkS1es',
+          });
 
-        // const files = [
-        //   new File([thumbnail], 'thumbnail.png'),
-        //   new File([metadata], 'metadata.json'),
-        // ];
-        // const cid = await web3Client.put(files);
+          const cid = await nftstorage.storeBlob(new Blob(['hello']));
 
-        // if (cid) {
-        //   console.log(cid);
+          // if (cid) {
+          // console.log(cid);
 
-        //   // const res = await fcl.mutate({
-        //   //   cadence: MINT_ARLEE_SCENE_NFT,
-        //   //   limit: 999,
-        //   //   args: (arg, t) => [
-        //   //     arg(cid, t.String),
-        //   //     arg('description', t.String),
-        //   //   ],
-        //   // });
-        //   // await tx(res).onceSealed();
-        // }
-      });
+          // const res = await fcl.mutate({
+          //   cadence: MINT_ARLEE_SCENE_NFT,
+          //   limit: 999,
+          //   args: (arg, t) => [
+          //     arg(cid, t.String),
+          //     arg('description', t.String),
+          //   ],
+          // });
+          // await tx(res).onceSealed();
+          // }
+        }
+      );
     }
   }, [unityContext]);
 
